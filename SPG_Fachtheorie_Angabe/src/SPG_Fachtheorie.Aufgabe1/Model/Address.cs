b39bfa4d76b9b5d8079a88e429f314edf0df2f74
BojanMarQ <mar220774@spengergasse.at>
2025-02-26 18:03:19 +0100

    efcore_wiederholung

---
+++ SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Model/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace SPG_Fachtheorie.Aufgabe1.Model
+{
+    public class Address
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        protected Address() { }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public Address(string street, string city, string zip) { 
+            Street = street;
+            City = city;
+            Zip = zip;
+        }
+        public int ID { get; set; }
+        public string Street {get;  }
+        public string City {get;  }
+        public string Zip {get; }
+    }
+}
---
+++ SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Model/Cashier.cs
@@ -0,0 +1,16 @@
+﻿namespace SPG_Fachtheorie.Aufgabe1.Model
+{
+    public class Cashier : Employee
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        protected Cashier() {}
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public Cashier(string jobSpezialisation, string firstName, string lastName, Address address): base ()
+        {
+            JobSpezialisation = jobSpezialisation;
+        }
+        public string JobSpezialisation { get; set; }
+
+
+    }
+}
